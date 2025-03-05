25d811d3bb0155af13976990cf93b01026486783
fsevindik <fsevindik2923@gmail.com>
2025-03-06 01:16:18 +0300

    feat(mail):add email.js

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,21 +1,24 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const Home = () => {
   return (
     <section
       id="home"
       className="min-h-screen flex items-center justify-center relative"
     >
-      <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r  from-blue-400 to-purle-600 
-        bg-clip-text text-transparent  gradient-text leading-right">
-            Hi, I'm Fırat Sevindik
-        </h1>
+      <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
+            Hi, I'm Pedro Tech
+          </h1>
 
-        <p className="text-gray-400 text-xl mb-8 max-w-lg mx-auto ">
-            I'm a passionate software developer who loves coffe.tsx more than typescript.I love crafting scalable and efficient web applications.
-            My goal is to become proficient full stack developer.
-        </p>
-        <div className="flex justify-center space-x-4">
-             <a
+          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            I’m a full-stack developer who loves crafting clean, scalable web
+            applications. My goal is to build solutions that offer both
+            exceptional performance and a delightful user experience.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
               href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
@@ -29,9 +32,9 @@ export const Home = () => {
             >
               Contact Me
             </a>
-
+          </div>
         </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
